30bcf8d3ad489663a1a3972b2fdbf86cb3c75e4a
Huanxiang Su <xiang625764563@gmail.com>
2020-04-22 19:47:32 -0700

    added subsection component to modal and optimized code

--- src/components/MediaModal.js
+++ src/components/MediaModal.js
@@ -7,14 +7,14 @@ import {
     Modal,
     Image,
     Icon,
-    Header,
     Label,
     Card,
     Loader,
     Segment,
     Dimmer,
     Grid,
-    Rating
+    Rating,
+    Transition
 } from "semantic-ui-react";
 import style from "./MediaList.module.scss";
 
@@ -27,9 +27,37 @@ class MediaModal extends React.Component {
         this.state = {
             id: null,
             banner: "",
-            media: null
+            media: null,
+            informationVisible: false,
+            statisticVisible: false,
+            characterListVisible: false,
+            watchListVisible: false,
         };
     }
+
+    toggleInformation = () => {
+        this.setState({
+            informationVisible: !this.state.informationVisible,
+        });
+    };
+
+    toggleStatistic = () => {
+        this.setState({
+            statisticVisible: !this.state.statisticVisible,
+        });
+    };
+
+    toggleCharacter = () => {
+        this.setState({
+            characterListVisible: !this.state.characterListVisible,
+        });
+    };
+
+    toggleWatch = () => {
+        this.setState({
+            watchListVisible: !this.state.watchListVisible,
+        });
+    };
 
     UNSAFE_componentWillReceiveProps(nextProps) {
         if (nextProps.open) {
@@ -38,17 +66,20 @@ class MediaModal extends React.Component {
             AnimeQuery.getMediaByID(nextProps.id).then((res) => {
                 this.setState({
                     id: nextProps.id,
-                    banner: res.data.Media.bannerImage,
-                    media: res.data.Media
+                    media: res.data.Media,
                 });
             });
         }
 
+        // reset all states when modal closed
         if (nextProps.reset) {
             this.setState({
                 id: null,
-                banner: "",
-                media: null
+                media: null,
+                informationVisible: false,
+                statisticVisible: false,
+                characterListVisible: false,
+                watchListVisible: false,
             });
         }
     }
@@ -57,14 +88,18 @@ class MediaModal extends React.Component {
         const { media } = this.state;
         if (media) {
             // description
-            var description = media.description.split('<br><br>');
+            let desList = media.description.split(/<br>*<br>/g);
+            const description = [];
+            for (var i = 0; i < desList.length; ++i) {
+                description.push(<p key={"description" + i}>{desList[i]}</p>);
+            }
 
             // score
-            var score = Math.round(media.meanScore / 100 * 10);
+            var score = Math.round((media.meanScore / 100) * 10);
 
             // tag list
             var tags = [];
-            media.tags.forEach(tag => {
+            media.tags.forEach((tag) => {
                 tags.push(
                     <Label as="a" tag key={tag.name} className={style.tag}>
                         {tag.name}
@@ -75,13 +110,20 @@ class MediaModal extends React.Component {
             // genres
             var color = media.coverImage.color;
             var genres = [];
-            media.genres.forEach(genre => {
+            media.genres.forEach((genre) => {
                 genres.push(
-                    <Label as="a" circular color="blue" key={genre} className={style.genre} size="big" >
+                    <Label
+                        as="a"
+                        circular
+                        color="black"
+                        key={genre}
+                        className={style.genre}
+                        size="big"
+                    >
                         {genre}
                     </Label>
                 );
-            })
+            });
 
             return (
                 <Modal
@@ -91,73 +133,118 @@ class MediaModal extends React.Component {
                     closeOnDimmerClick={true}
                     // closeIcon
                 >
-                    <div className={style.modalBanner}>
-                        <img
-                            src={this.state.banner}
-                            alt={media.title.english}
-                            className={style.modalBannerImg}
-                        ></img>
-                        {/* <div style={{backgroundImage: "url("+this.state.banner+")"}} className={style.modalBannerImg}></div> */}
-                    </div>
+                    <div className={style.myModal}>
+                        {media.bannerImage ? (
+                            <div className={style.modalBanner}>
+                                <img
+                                    src={media.bannerImage}
+                                    alt={media.title.english}
+                                    className={style.modalBannerImg}
+                                ></img>
+                            </div>
+                        ) : null}
 
-                    <Container className={style.modalContent}>
-                        <div className={style.modalTitle}>
-                            <div className={style.modalNative}>
-                                {media.title.native}
+                        <Container className={style.modalContent}>
+                            <div className={style.modalTitle}>
+                                <div className={style.modalNative}>
+                                    {media.title.native}
+                                </div>
+                                <div className={style.modalRomaji}>
+                                    ({media.title.romaji})
+                                </div>
+                                <div className={style.modalEnglish}>
+                                    {media.title.english}
+                                </div>
                             </div>
-                            <div className={style.modalRomaji}>
-                                ({media.title.romaji})
-                            </div>
-                            <div className={style.modalEnglish}>
-                                {media.title.english}
-                            </div>
-                        </div>
 
-                        <Grid columns={2} divided>
-                            <Grid.Row>
-                                <Grid.Column width={7}>
-                                    <div className={style.cover}>
-                                        <Image
-                                            src={media.coverImage.large}
-                                            size="medium"
-                                            centered
-                                            rounded
-                                        />
-                                    </div>
-                                    <div className={style.rating}>
-                                        <Rating
-                                            maxRating={10}
-                                            defaultRating={score}
-                                            disabled
-                                            icon="star"
-                                            size="large"
-                                            title={
-                                                "Mean Score: " + media.meanScore
+                            <Grid columns={2} divided>
+                                <Grid.Row>
+                                    <Grid.Column width={7}>
+                                        <div className={style.cover}>
+                                            <Image
+                                                src={media.coverImage.large}
+                                                size="medium"
+                                                centered
+                                                rounded
+                                            />
+                                        </div>
+                                        <div className={style.rating}>
+                                            <Rating
+                                                maxRating={10}
+                                                defaultRating={score}
+                                                disabled
+                                                icon="star"
+                                                size="large"
+                                                title={
+                                                    "Mean Score: " +
+                                                    media.meanScore
+                                                }
+                                                className={style.ratingSize}
+                                            />
+                                        </div>
+                                        <div className={style.genreContainer}>
+                                            {genres}
+                                        </div>
+                                    </Grid.Column>
+                                    <Grid.Column width={9}>
+                                        <div
+                                            className={
+                                                style.flexColumnContainer
                                             }
-                                            className={style.ratingSize}
-                                        />
-                                    </div>
-                                    <div className={style.genreContainer}>
-                                        {genres}
-                                    </div>
-                                </Grid.Column>
-                                <Grid.Column width={9}>
-                                    <div className={style.flexColumnContainer}>
-                                        <div className={style.description}>
-                                            <p>
-                                                <b>Description:</b>
-                                            </p>
-                                            <p>{description[0]}</p>
-                                            <p>{description[1]}</p>
+                                        >
+                                            <div className={style.description}>
+                                                <p>
+                                                    <b>Description:</b>
+                                                </p>
+                                                {description}
+                                            </div>
+                                            <div
+                                                className={style.tagsContainer}
+                                            >
+                                                {tags}
+                                            </div>
                                         </div>
-                                        <div className={style.tagsContainer}>
-                                            {tags}
-                                        </div>
-                                    </div>
-                                </Grid.Column>
-                            </Grid.Row>
-                        </Grid>
-                    </Container>
+                                    </Grid.Column>
+                                </Grid.Row>
+                            </Grid>
+
+                            <SubSection
+                                click={this.toggleInformation}
+                                visible={this.state.informationVisible}
+                                name="Media Information"
+                                color="blue"
+                            >
+                                <Image size="large" src={paragraph} />
+                            </SubSection>
+
+                            <SubSection
+                                click={this.toggleStatistic}
+                                visible={this.state.statisticVisible}
+                                name="Media Statistic"
+                                color="olive"
+                            >
+                                <Image size="large" src={paragraph} />
+                            </SubSection>
+
+                            <SubSection
+                                click={this.toggleCharacter}
+                                visible={this.state.characterListVisible}
+                                name="Character List"
+                                color="violet"
+                            >
+                                <Image size="large" src={paragraph} />
+                            </SubSection>
+
+                            <SubSection
+                                click={this.toggleWatch}
+                                visible={this.state.watchListVisible}
+                                name="Watch List"
+                                color="teal"
+                            >
+                                <Image size="large" src={paragraph} />
+                            </SubSection>
+                        </Container>
+                    </div>
 
                     <Modal.Actions>
                         <Button negative onClick={this.props.close}>
@@ -166,7 +253,6 @@ class MediaModal extends React.Component {
                     </Modal.Actions>
                 </Modal>
             );
-
         } else {
             return (
                 <Modal
@@ -199,6 +285,37 @@ class MediaModal extends React.Component {
             );
         }
     }
+}
+
+
+const SubSection = (props) => {
+    return (
+        <div className={style.informationContainer}>
+            <Button
+                onClick={props.click}
+                className={style.modalBigBtn}
+                color={props.color}
+                circular
+                icon
+                labelPosition="right"
+            >
+                {props.name}
+                {props.visible ? (
+                    <Icon name="angle double up" />
+                ) : (
+                    <Icon name="angle double down" />
+                )}
+            </Button>
+            <Transition
+                visible={props.visible}
+                animation="slide down"
+                duration={500}
+                unmountOnHide={true}
+            >
+                {props.children}
+            </Transition>
+        </div>
+    );
 }
 
 export default MediaModal;
